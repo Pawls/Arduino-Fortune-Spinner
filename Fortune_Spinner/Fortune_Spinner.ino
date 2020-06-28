581fc007c84f4a464ad24e9a31d1659eaceba60c
cantuc40 <44282951+cantuc40@users.noreply.github.com>
2020-06-28 22:26:15 +0000

    Fortune_spinner
    Added the notes and pitch header files and deleted the buzzer test code. Each fortune now has its own melody.

--- Fortune_Spinner/Fortune_Spinner.ino
+++ Fortune_Spinner/Fortune_Spinner.ino
@@ -1,6 +1,9 @@
 #include <Wire.h>
 #include <Stepper.h>
 #include <LiquidCrystal.h>
+#include <Notes.h>
+#include <pitches.h>
+
 
 //Pin definitions
 const int button_pin = 12;
@@ -40,48 +43,55 @@ void loop() {
 
 void displayFortune(int roll){
   lcd.clear();
-  tone(buzz_pin, 261, 100);
   delay(200);
   switch(roll){
     case 0:
       lcd.print("ROLLED FORTUNE 1");
       lcd.setCursor(0,1);
       lcd.print("Learn from mistakes to improve");
+      fortune1(buzz_pin);
       break;
     case 1:
       lcd.print("ROLLED FORTUNE 2");
       lcd.setCursor(0,1);
       lcd.print("It is not unwise to ask for help");
+      fortune2(buzz_pin);
       break;
     case 2:
       lcd.print("ROLLED FORTUNE 3");
       lcd.setCursor(0,1);
       lcd.print("Practice makes perfect");
+      fortune3(buzz_pin);
       break;
     case 3:
       lcd.print("ROLLED FORTUNE 4");
       lcd.setCursor(0,1);
       lcd.print("Knowledge comes with time");
+      fortune4(buzz_pin);
       break;
     case 4:
       lcd.print("ROLLED FORTUNE 5");
       lcd.setCursor(0,1);
       lcd.print("Give first in order to take");
+      fortune5(buzz_pin);
       break;
     case 5:
       lcd.print("ROLLED FORTUNE 6");
       lcd.setCursor(0,1);
       lcd.print("Good things can be unexpected");
+      fortune6(buzz_pin);
       break;
     case 6:
       lcd.print("ROLLED FORTUNE 7");
       lcd.setCursor(0,1);
       lcd.print("It is time to try something new");
+      fortune7(buzz_pin);
       break;
     case 7:
       lcd.print("ROLLED FORTUNE 8");
       lcd.setCursor(0,1);
       lcd.print("Welcome changes in your life");
+      fortune8(buzz_pin); 
       break;
   }
   delay(1000);
